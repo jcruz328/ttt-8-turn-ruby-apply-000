45952b896e44b820b22e0fb5fdf7816f136f2ba4
Joel <jcruz328@gmail.com>
2020-09-08 05:44:47 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,37 @@
+board = 
+def turn
+  puts "Please enter 1-9:"
+  input = gets.strip
+  corrected_input = input_to_index(input)
+  
+  if valid_move?(board, corrected_input)
+    move(board,corrected_input, token)
+    display_board(board)
+  else
+    turn
+  end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  if board[index] == " " && index.between?(0,8)
+    true
+  else
+    false
+  end
+end
+
+def move(board, index, token = "X")
+  board[index] = token
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
